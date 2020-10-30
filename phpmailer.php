479477cf9b5d4e3406a1f5dd440301d451015ad1
Sayidazim <sayidazimahmudov@gmail.com>
2020-10-30 21:34:34 +0500

    SMTP server added

--- phpmailer.php
+++ phpmailer.php
@@ -2,6 +2,7 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 require_once "vendor/autoload.php";
 
@@ -9,6 +10,8 @@ class Mail
 {
     const TO_ADDRESS = 'sam1997co@mail.ru';
     const TO_NAME = 'Sayidazim Mahmudov';
+    const MAIL_USERNAME = ''; // full gmail address
+    const MAIL_PASSWORD = '';
 
     protected $params = ['name', 'email', 'phone', 'category', 'lang', 'purpose', 'visited'];
     protected $request = []; // POST data
@@ -36,17 +39,44 @@ class Mail
      */
     public function send()
     {
-        $mail = new PHPMailer(true); // Argument true in constructor enables exceptions
+        // Create a new PHPMailer instance
+        $mail = new PHPMailer(true);
 
-        // From email address and name
-        $mail->From = $this->request['email'];
-        $mail->FromName = $this->request['name'];
+        // Tell PHPMailer to use SMTP
+        $mail->isSMTP();
 
-        // To address and name
+        // Enable SMTP debugging
+        // SMTP::DEBUG_OFF = off (for production use)
+        // SMTP::DEBUG_CLIENT = client messages
+        // SMTP::DEBUG_SERVER = client and server messages
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+
+        // Set the hostname of the mail server
+        $mail->Host = 'smtp.gmail.com';
+
+        // Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+        $mail->Port = 587;
+
+        // Set the encryption mechanism to use - STARTTLS or SMTPS
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        // Whether to use SMTP authentication
+        $mail->SMTPAuth = true;
+
+        // Username to use for SMTP authentication
+        $mail->Username = self::MAIL_USERNAME;
+
+        // Password to use for SMTP authentication
+        $mail->Password = self::MAIL_PASSWORD;
+
+        // Set who the message is to be sent from
+        $mail->setFrom($this->request['email'], $this->request['name']);
+
+        // Set an alternative reply-to address
+        $mail->addReplyTo($this->request['email'], $this->request['name']);
+
+        // Set who the message is to be sent to
         $mail->addAddress(self::TO_ADDRESS, self::TO_NAME);
-
-        // Address to which recipient will reply
-        $mail->addReplyTo(self::TO_ADDRESS, self::TO_NAME);
 
         // Send HTML or Plain Text email
         $mail->isHTML(true);
